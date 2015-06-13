70b4ab00e735e065609c197d2a91654c07049ccd
Tim Oliver <me@timoliver.com.au>
2015-06-13 13:55:19 -0700

    Added attributes class to sample project.

--- TOCropViewController/Models/TOCroppedImageAttributes.h
+++ TOCropViewController/Models/TOCroppedImageAttributes.h
@@ -21,6 +21,7 @@
 //  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface TOCroppedImageAttributes : NSObject
 
