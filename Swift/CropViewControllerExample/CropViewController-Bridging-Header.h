43bd295f69eafefa9d6bf377693f1968f7e917de
Tim Oliver <me@timoliver.com.au>
2018-07-24 00:24:19 +0900

    Moved bridging header to sample app project

---
+++ Swift/CropViewControllerExample/CropViewController-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  CropViewController-Bridging-Header.h
+//  CropViewControllerExample
+//
+//  Created by Tim Oliver on 23/7/18.
+//  Copyright © 2018 Tim Oliver. All rights reserved.
+//
+
+#import "TOCropViewController.h"
