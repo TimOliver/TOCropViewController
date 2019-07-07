4fe9b48e98ffa7531fc3df6f374ee861458727b2
Tim Oliver <me@timoliver.com.au>
2019-07-08 00:17:09 +0900

    Added visible cancel button property to toolbar

--- Objective-C/TOCropViewController/Views/TOCropToolbar.h
+++ Objective-C/TOCropViewController/Views/TOCropToolbar.h
@@ -35,24 +35,25 @@ NS_ASSUME_NONNULL_BEGIN
 
 /* The 'Done' buttons to commit the crop. The text button is displayed
  in portrait mode and the icon one, in landscape. */
-@property (nonnull, nonatomic, strong, readonly) UIButton *doneTextButton;
-@property (nonnull, nonatomic, strong, readonly) UIButton *doneIconButton;
-@property (nonnull, nonatomic, copy) NSString *doneTextButtonTitle;
+@property (nonatomic, strong, readonly) UIButton *doneTextButton;
+@property (nonatomic, strong, readonly) UIButton *doneIconButton;
+@property (nonatomic, copy) NSString *doneTextButtonTitle;
 
 
 /* The 'Cancel' buttons to cancel the crop. The text button is displayed
  in portrait mode and the icon one, in landscape. */
-@property (nonnull, nonatomic, strong, readonly) UIButton *cancelTextButton;
-@property (nonnull, nonatomic, strong, readonly) UIButton *cancelIconButton;
-@property (nonnull, nonatomic, copy) NSString *cancelTextButtonTitle;
+@property (nonatomic, strong, readonly) UIButton *cancelTextButton;
+@property (nonatomic, strong, readonly) UIButton *cancelIconButton;
+@property (nonatomic, readonly) UIView *visibleCancelButton;
+@property (nonatomic, copy) NSString *cancelTextButtonTitle;
 
 /* The cropper control buttons */
-@property (nonnull, nonatomic, strong, readonly)  UIButton *rotateCounterclockwiseButton;
-@property (nonnull, nonatomic, strong, readonly)  UIButton *resetButton;
-@property (nonnull, nonatomic, strong, readonly)  UIButton *clampButton;
+@property (nonatomic, strong, readonly)  UIButton *rotateCounterclockwiseButton;
+@property (nonatomic, strong, readonly)  UIButton *resetButton;
+@property (nonatomic, strong, readonly)  UIButton *clampButton;
 @property (nullable, nonatomic, strong, readonly) UIButton *rotateClockwiseButton;
 
-@property (nonnull, nonatomic, readonly) UIButton *rotateButton; // Points to `rotateCounterClockwiseButton`
+@property (nonatomic, readonly) UIButton *rotateButton; // Points to `rotateCounterClockwiseButton`
 
 /* Button feedback handler blocks */
 @property (nullable, nonatomic, copy) void (^cancelButtonTapped)(void);
