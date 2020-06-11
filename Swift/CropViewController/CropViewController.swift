3e5d0055bd11e544b9a9c77ad1d8378268eb3c3c
Tim Oliver <me@timoliver.com.au>
2020-06-11 14:24:41 +0800

    Removed un-needed module import

--- Swift/CropViewController/CropViewController.swift
+++ Swift/CropViewController/CropViewController.swift
@@ -20,8 +20,6 @@
 //  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 //  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import TOCropViewController
-
 /**
  An enum containing all of the aspect ratio presets that this view controller supports
  */
