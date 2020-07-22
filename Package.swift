2ca59fe1d8f01f6b6ad420fe6a8723e89d3a455f
Grigoryan, Harout <haroutyun.grigoryan.864@my.csun.edu>
2020-07-21 23:23:02 -0700

    Update package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version:5.2.4
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "TOCropViewController",
-	defaultLocalization: "en",
     platforms: [.iOS(.v8)],
     products: [
         .library(
