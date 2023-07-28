38f2cdf36be7cb6a64435dcf74d931fe44a45d2c
milan-galitein <milan@galitein.in>
2023-07-28 12:59:28 +0530

    - Add crop preset 16*6

--- Objective-C/TOCropViewController/TOCropViewController.m
+++ Objective-C/TOCropViewController/TOCropViewController.m
@@ -660,6 +660,8 @@ static const CGFloat kTOCropViewControllerToolbarHeight = 44.0f;
             break;
         case TOCropViewControllerAspectRatioPreset16x9:
             aspectRatio = CGSizeMake(16.0f, 9.0f);
+        case TOCropViewControllerAspectRatioPreset16x6:
+            aspectRatio = CGSizeMake(16.0f, 6.0f);
             break;
         case TOCropViewControllerAspectRatioPresetCustom:
             aspectRatio = self.customAspectRatio;
