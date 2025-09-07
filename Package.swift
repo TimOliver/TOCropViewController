96b82648f12115bf881bcb123344a7788430de24
Robert Ryan <robert.ryan@mindspring.com>
2025-09-07 11:19:15 -0700

    Bump minimum OS version
    The `Package.swift` file had a minimum OS version of iOS 11, but Xcode 26 requires a min OS version not less than iOS 12.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "TOCropViewController",
     defaultLocalization: "en",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "TOCropViewController",
